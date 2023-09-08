be092afc3aaaaac9d49b9fcf2ab6e1d9f1b87dd1
Renan Pinheiro <renan.coelho.p@gmail.com>
2023-09-07 22:12:26 -0300

    refactor: move guicontroller to folder

--- src/App.java
+++ src/App.java
@@ -1,6 +1,7 @@
 import communication.communicator.Communicator;
 import communication.communicator.socket.SocketCommunicator;
 import controllers.Controller;
+import controllers.gui.GUIController;
 import game.match.Game;
 import game.match.MatchBuilder;
 
--- src/controllers/gui/GUIController.java
+++ src/controllers/gui/GUIController.java
@@ -1,3 +1,5 @@
+package controllers.gui;
+
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
